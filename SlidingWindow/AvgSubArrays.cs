301b83578d4382b7f23270de3ffd7c631aaea792
Buse Duran <buseduran.me@gmail.com>
2024-12-06 09:30:02 +0300

    Find Averages of Sub Arrays

---
+++ SlidingWindow/AvgSubArrays.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeQuest.SlidingWindow
+{
+    public class AvgSubArrays
+    {
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            int maxSum = nums.Take(k).Sum();
+            int sum = maxSum;
+            for (int i = k ; i < k ; i++)
+            {
+                maxSum = maxSum + nums[i] - nums[i - k];
+                if (maxSum < sum)
+                    maxSum = sum;
+            }
+            return ( double )maxSum / k;
+        }
+    }
+}
